7c2e0a88f9fdf83e5a11388017ea461fe4995eef
Luis Furnas <luisfurnas@gmail.com>
2019-12-13 00:29:36 -0100

    Add comments for both VertXIO trait and OpenApiRouting trait

--- src/main/scala/vertx/zio/OpenApiRouting.scala
+++ src/main/scala/vertx/zio/OpenApiRouting.scala
@@ -9,10 +9,22 @@ import zio._
 trait OpenApiRouting extends VertXIO {
   self: ScalaVerticle =>
 
+  /**
+   * OpenAPI module.
+   */
   trait OpenApi {
     val specPath: UIO[String]
   }
 
+  /**
+   * Describes the effect of creating a router factory.
+   * Gets the Java version of the VertX and the spec path, and creates an Open API 3 router factory.
+   *
+   * Since the creation of the factory requires an async result callback, `effectAsync` is used to tranform it into a ZIO effect.
+   * This way, the result is referentially transparent and easier to reason with. Also it can be combined with other effects.
+   *
+   * @return An effect that requires a VertX with OpenApi, may fail with Throwable or succeed with an OA3RouterFactory.
+   */
   def createRouterFactory: RIO[VertX with OpenApi, OA3RouterFactory] = {
     for {
       vertx <- ZIO.access[VertX](_.vertx)
@@ -30,5 +42,13 @@ trait OpenApiRouting extends VertXIO {
     } yield routerFactory
   }
 
+  /**
+   * Describes the effect of creating a router, given an Open API 3 router factory.
+   * Must be implemented by the implementer of this trait.
+   * This way it's easier to create a router with custom options, like security handlers for example.
+   *
+   * @param routerFactory Router factory to use for the creation of the router.
+   * @return An effect that requires a VertX, may fail with Throwable or succeed with a Router.
+   */
   def createRouter(routerFactory: OA3RouterFactory): RIO[VertX, Router]
 }
--- src/main/scala/vertx/zio/VertXIO.scala
+++ src/main/scala/vertx/zio/VertXIO.scala
@@ -14,25 +14,61 @@ trait VertXIO {
 
   type Config = JsonObject
 
+  /**
+   * VertX trait module.
+   */
   trait VertX {
     val vertx: VertX.Service[Any]
   }
 
+  /**
+   * VertX module companion, containing everything necessary to create servers, deploy and undeploy verticles.
+   * Also provides a deployment id, which identifies a deployed verticle, and is used to undeploy it,
+   * and also provides a configuration object, to access properties like host and port.
+   */
   object VertX {
 
     trait Service[R] {
+      /**
+       * Deployment ID of the deployed vertice, used for undeployment.
+       */
       val deploymentId: UIO[String]
+      /**
+       * Configuration object.
+       */
       val config: UIO[Config]
 
+      /**
+       * Creates an HTTP server.
+       *
+       * @return An effect with R requirements, that may fail with Throwable or succeed with an HttpServer.
+       */
       def createHttpServer: RIO[R, HttpServer]
 
+      /**
+       * Undeploys the verticle given the deployment ID.
+       *
+       * @param deploymentId Deployment ID of the deployed verticle.
+       * @return An effect with R requirements, that may fail with Throwable or succeed with Unit.
+       */
       def undeployFuture(deploymentId: String): RIO[R, Unit]
 
+      /**
+       * Converts the VertX to it's Java version.
+       * Used for integration with core VertX methods which require the Java version.
+       *
+       * @return An effect with R requirements, that may fail with Throwable or succeed with a Java version of VertX.
+       */
       def asJava: RIO[R, JVertx]
     }
 
   }
 
+  /**
+   * VertX live module.
+   * It uses verticle properties to fill the module properties, and uses the vertx instance for the methods.
+   * This module acts like a proxy for VertX features.
+   */
   trait VertXLive extends VertX {
     final val vertx: VertX.Service[Any] = new VertX.Service[Any] {
       val deploymentId: UIO[String] = UIO(self.deploymentID)
@@ -46,6 +82,14 @@ trait VertXIO {
     }
   }
 
+  /**
+   * Describes the effect of creating an HTTP server, given a request handler.
+   * Creates an HTTP server, attaches the router and then listens on the host and port, supplied by the config.
+   * Success or failure are logged using the Logger provided by the environment.
+   *
+   * @param router VertX router to handle requests
+   * @return An effect that requires a VertX with Logger, may fail with Throwable or succeed with an HttpServer.
+   */
   final def createHttpServer(router: Router): RIO[VertX with Logger, HttpServer] = {
     val serverConfig = (vertx: VertX.Service[Any]) => for {
       config <- vertx.config
@@ -64,6 +108,12 @@ trait VertXIO {
     } yield startedServer
   }
 
+  /**
+   * Describes the effect of undeploying a verticle.
+   * Fetches the vertx instance and the deployment id from the environment, then proceeds to undeploy the verticle.
+   *
+   * @return An effect that requires a VertX, may fail with Throwable or succeed with Unit.
+   */
   final def undeployVerticle: RIO[VertX, Unit] = {
     for {
       vertx <- ZIO.access[VertX](_.vertx)
